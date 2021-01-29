b345932917ca27181dd762e1012338082bcea4d8
Thomas <ducret.thomas@gmail.com>
2021-01-29 16:02:16 +0100

    Begin Config

---
+++ src/fr/kosmosuniverse/kuffle/Core/Config.java
@@ -0,0 +1,14 @@
+package fr.kosmosuniverse.kuffle.Core;
+
+public class Config {
+	public boolean saturation;
+	public boolean spread;
+	public boolean rewards;
+	public boolean skip;
+	public int skipAge;
+	public int maxAges;
+	public int startTime;
+	public int addedTime;
+	public String lang;
+	public Level level;
+}
---
+++ src/fr/kosmosuniverse/kuffle/Core/Level.java
@@ -0,0 +1,8 @@
+package fr.kosmosuniverse.kuffle.Core;
+
+public enum Level {
+	EASY,
+	NORMAL,
+	EXPERT,
+	ULTRA
+}
